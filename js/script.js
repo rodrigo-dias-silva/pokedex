91ba0348c9f4dd039c1baf1846f6d246ad98e702
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-11 23:42:08 -0300

    finalizaçao, config do JS e funcionalidades dos botoes

--- js/script.js
+++ js/script.js
@@ -2,21 +2,59 @@ const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonImg = document.querySelector('.pokemon-img');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-src');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 async function fetchPokemon(pokemon) {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-  const data = await APIResponse.json();
-
-  return data;
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
 }
 
 async function renderPokemon(pokemon) {
+  pokemonName.innerHTML = 'Procurando...';
+  pokemonNumber.innerHTML = '';
+
   const data = await fetchPokemon(pokemon);
 
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  if (data) {
+    pokemonImg.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
+    input.value = '';
+
+    searchPokemon = data.id;
+  } else {
+    pokemonImg.style.display = 'none';
+    pokemonName.innerHTML = 'Meteu essa, fera?!';
+    pokemonNumber.innerHTML = '';
+  }
 }
 
-renderPokemon(2);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  renderPokemon(input.value.toLowerCase());
+});
+
+btnPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+btnNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
